adccd65cf2d42cf6b53d63359b96c2a07fdd18c0
Ravi Ranjan Ojha <raviranjanojha@Ravis-MacBook-Pro.local>
2022-04-08 14:59:16 +0530

    checked valid token

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/auth/authControllers');
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
+const auth = require('../middleware/auth')
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
@@ -17,5 +18,10 @@ const loginSchema = Joi.object({
 
 router.post('/register', validator.body(registerSchema), authControllers.controllers.postRegister);
 router.post('/login', validator.body(loginSchema),authControllers.controllers.postLogin);
+
+// test route to verify if middleware is working
+router.get('/test', auth, (req,res) => {
+    res.send('request passed');
+});
 
 module.exports = router;
