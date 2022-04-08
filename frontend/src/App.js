6418c3f02bd2b488aee3958c1d23f4c3776647ba
RAVI RANJAN OJHA <42292155+ravirojha@users.noreply.github.com>
2022-04-08 19:13:34 +0530

    Renamed folders

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";import './App.css';
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
+import NotFound from "./Not-Found/Not-Found";
+
+function App() {
+  return (
+    <>
+    <Router>
+        <Routes>
+            <Route path={'/login'} element={<LoginPage />}/>
+            <Route path={'/register'} element={<RegisterPage />}/>
+            <Route path={'/dashboard'} element={<Dashboard />}/>
+            <Route path={'/'} element={<Dashboard />}/>
+            <Route path={'*'} element={<NotFound />}/>
+        </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Not-Found/Not-Found.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function NotFound(props) {
+    return (
+        <h1>Not Found</h1>
+    );
+}
+
+export default NotFound;
