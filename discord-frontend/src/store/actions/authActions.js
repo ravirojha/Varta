e89a50b6f68a7d5837d4de785afc5a76b3820f15
Ravi Ranjan Ojha <raviranjanojha@Ravis-MacBook-Pro.local>
2022-04-08 18:58:12 +0530

    Created login and register actions

--- discord-frontend/src/store/actions/authActions.js
+++ discord-frontend/src/store/actions/authActions.js
@@ -1,6 +1,6 @@
 import * as api from '../../api'
 
-const authActions = {
+export const authActions = {
     SET_USER_DETAILS: 'AUTH_SET_USER_DETAILS'
 };
 
@@ -8,7 +8,7 @@ export const getActions = (dispatch) => {
     return {
         login: (userDetails, navigate) => dispatch(login(userDetails, navigate)),
         register: (userDetails, navigate) =>
-            dispatch(register(userDetails, history)),
+            dispatch(register(userDetails, navigate)),
     }
 }
 
@@ -20,7 +20,7 @@ const setUserDetails = (userDetails) => {
 };
 
 
-const login = (userDetails, navigate) => {
+export const login = (userDetails, navigate) => {
     return async (dispatch) => {
         const response = await api.login(userDetails);
 
@@ -28,11 +28,28 @@ const login = (userDetails, navigate) => {
             // show error message in alert
 
         } else {
-            const { userDetails } response.data;
-            localStorage.setItem('user', JSON.stringify('userDetails'));
+            const { userDetails } = response?.data;
+            localStorage.setItem('user', JSON.stringify(userDetails));
 
             dispatch(setUserDetails(userDetails));
-            navigate(/dashboard);
+            navigate('/dashboard');
+        }
+    }
+}
+
+export const register = (userDetails, navigate) => {
+    return async (dispatch) => {
+        const response = await api.register(userDetails);
+
+        if(response.error) {
+            // show error message in alert
+
+        } else {
+            const { userDetails } = response?.data;
+            localStorage.setItem('user', JSON.stringify(userDetails));
+
+            dispatch(setUserDetails(userDetails));
+            navigate('/dashboard');
         }
     }
 }
